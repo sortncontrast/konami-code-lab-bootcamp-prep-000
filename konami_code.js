400887ca77addc4980da9e732a7096e4048cef6c
tonyamagar@gmail.com <tonyamagar@gmail.com>
2018-05-02 02:29:05 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,20 +3,18 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init() {
- function onKeyDownHandler(e) {
-   const key = parseInt(e.detail || e.which);
-   
-   if(key === code[index]) {
-     index += 1;
-     
-     if(index === code.length) {
-       alert("Hurray!");
-       
-       index = 0;
-     }
-   }else {
-     index = 0;
-   }
- }
-
+  document.addEvenListener("keydown", function(e) {
+    const key = parseInt(e.detail || e.which);
+    
+    if(key === code[index]) {
+      index += 1;
+      
+      if(index === code.length - 1) {
+        window.alert("Woo Hoo!!!");
+        index = 0;
+      }
+    }else {
+      index = 0;
+    }
+  });
 }
