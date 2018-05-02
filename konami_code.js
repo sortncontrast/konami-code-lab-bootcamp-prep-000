737891952dc0d80e8fb0f24340e273e6f7ca2fb4
tonyamagar@gmail.com <tonyamagar@gmail.com>
2018-05-02 02:31:04 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,8 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let index = 0;
+
 function init() {
-  var index = 0;
   document.addEventListener("keydown", function(e) {
     const key = parseInt(e.detail || e.which);
     
